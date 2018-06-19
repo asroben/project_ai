ea43a1793ac660b6eef0a1c3cc7d0fa61f55045e
michaelqw <mic333@live.nl>
2018-06-19 17:37:21 +0200

    added freeze-weights option true/false

---
+++ classifier_new.py
@@ -0,0 +1,327 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# License: BSD
+# Author: Sasank Chilamkurthy
+
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import logging
+
+plt.ion()   # interactive mode
+
+
+# ### Model Settings
+
+# In[2]:
+
+
+# SETTINGS
+FREEZE_WEIGHTS = False
+BATCH_SIZE = 10
+NUM_CLASSES = 25
+NUM_EPOCHS = 1
+
+# Local Directory
+data_dir = 'processed_data'
+
+# Directory on DAS4
+#data_dir = '/var/scratch/prai1809/processed_data'
+
+
+# In[3]:
+
+
+# The CNN we use wants input dimensions of 3x224x224
+#
+# We use dataset of 2D faces where width x height is not 224x224
+#  -> Rescale to 256 as first transformation
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+# Data folder structure:
+# /augmented_dataset
+#   - /train
+#       - /class_1
+#       - /class_2
+#   - /val
+#       - /class_1
+#       - /class_2
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=BATCH_SIZE,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+# In[4]:
+
+
+# All classes
+image_datasets['train'].classes
+
+
+# ### Logging
+
+# In[5]:
+
+
+model_filename = 'FREEZEWEIGHTS_{}_CLASSES_{}_BATCHSIZE_{}_EPOCHS_{}_TRAIN_{}_VAL_{}'.format(FREEZE_WEIGHTS, NUM_CLASSES, BATCH_SIZE,NUM_EPOCHS, dataset_sizes['train'], dataset_sizes['val']  )
+
+LOG_FILENAME = model_filename + ".log"
+logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
+logging.info(LOG_FILENAME)
+print(LOG_FILENAME)
+
+
+# In[6]:
+
+
+# COMMENT OUT WHEN RUNNING SCRIPT ON SERVER
+
+# def imshow(inp, title=None):
+#     """Imshow for Tensor."""
+#     inp = inp.numpy().transpose((1, 2, 0))
+#     mean = np.array([0.485, 0.456, 0.406])
+#     std = np.array([0.229, 0.224, 0.225])
+#     inp = std * inp + mean
+#     inp = np.clip(inp, 0, 1)
+#     plt.imshow(inp)
+#     if title is not None:
+#         plt.title(title)
+#     plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+# # Get a batch of training data
+# inputs, classes = next(iter(dataloaders['train']))
+
+# # Make a grid from batch
+# out = torchvision.utils.make_grid(inputs)
+
+# imshow(out, title=[class_names[x] for x in classes])
+
+
+# In[7]:
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        logging.info('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        
+        print('-' * 10)
+        logging.info('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            logging.info('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format( time_elapsed // 60, time_elapsed % 60))
+    logging.info('Training complete in {:.0f}m {:.0f}s'.format( time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    logging.info('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+# In[8]:
+
+
+# def visualize_model(model, num_images=6):
+#     was_training = model.training
+#     model.eval()
+#     images_so_far = 0
+#     fig = plt.figure()
+
+#     with torch.no_grad():
+#         for i, (inputs, labels) in enumerate(dataloaders['val']):
+#             inputs = inputs.to(device)
+#             labels = labels.to(device)
+
+#             outputs = model(inputs)
+#             _, preds = torch.max(outputs, 1)
+
+#             for j in range(inputs.size()[0]):
+#                 images_so_far += 1
+#                 ax = plt.subplot(num_images//2, 2, images_so_far)
+#                 ax.axis('off')
+#                 ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+#                 imshow(inputs.cpu().data[j])
+
+#                 if images_so_far == num_images:
+#                     model.train(mode=was_training)
+#                     return
+#         model.train(mode=was_training)
+
+
+# In[ ]:
+
+
+if FREEZE_WEIGHTS == True:
+    # Settings with frozen weights
+    
+    model_conv = torchvision.models.resnet18(pretrained=True)
+    for param in model_conv.parameters():
+        param.requires_grad = False
+
+    # Parameters of newly constructed modules have requires_grad=True by default
+    num_ftrs = model_conv.fc.in_features
+    model_conv.fc = nn.Linear(num_ftrs, NUM_CLASSES)
+
+    model_conv = model_conv.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that only parameters of final layer are being optimized as
+    # opoosed to before.
+    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+else:
+    # Settings where all weights will be trained
+    
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, NUM_CLASSES)
+
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+
+# In[ ]:
+
+
+if FREEZE_WEIGHTS == True:
+    model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=NUM_EPOCHS)
+else:
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=NUM_EPOCHS)
+
+
+# ### Save Model
+
+# In[ ]:
+
+
+if FREEZE_WEIGHTS == True:
+    torch.save(model_conv, model_filename)
+else:
+    torch.save(model_ft, model_filename)
+
+
+# In[ ]:
+
+
+# COMMENT OUT BEFORE RUNNING ON SERVER
+# visualize_model(model_conv)
+
+# plt.ioff()
+# plt.show()
+
+
+# # Convert Notebook to Python Script to use on server
+# Delete sections after this before running on server
+
+# In[27]:
+
+
+get_ipython().system('jupyter nbconvert --to script classifier_new.ipynb')
+
+
+# In[ ]:
+
+
+
+
